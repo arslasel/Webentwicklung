f83a3be0d93a5eeef223c58c6a8ca11ceaceff7a
Selim Arslan <arslasel@students.zhaw.ch>
2021-11-03 21:11:17 +0100

    added Task2

--- Praktikum7/currentTemp.js
+++ Praktikum7/currentTemp.js
@@ -1,25 +1,24 @@
-const https = require('https')
+var https = require('https');
+let city = process.argv.slice(2)[0]
 
-const get = async (url) => {
-    return new Promise((resolve) => {
-        https.get(url, result => {
-            let data = '';
+//The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
+var options = {
+  host: 'wttr.in',
+  path: '/' + city + '?format=j1'
+};
 
-            result.on('data', chunk => { data += chunk })
-            result.on('end', () => {
-                resolve(JSON.parse(data));
-            })
-        });
-    });
+callback = function(response) {
+  var str = '';
+
+  //another chunk of data has been received, so append it to `str`
+  response.on('data', function (chunk) {
+    str += chunk;
+  });
+
+  //the whole response has been received, so we just print it out here
+  response.on('end', function () {
+    console.log(JSON.parse(str).current_condition[0].temp_C + '°');
+  });
 }
 
-const getTemperature = async (location) => {
-    const data = await get(`https://wttr.in/${location}?format=j1`);
-    return data['current_condition'][0]['temp_C'];
-}
-
-const args = process.argv.slice(2);
-
-getTemperature(args[0]).then(temperature => {
-    console.log(`${temperature}° C`);
-});
+https.get(options, callback);
