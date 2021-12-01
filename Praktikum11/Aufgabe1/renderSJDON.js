e8497d2a4339769c469bb6e9d9628809fa7405e2
Selim Arslan <arslasel@students.zhaw.ch>
2021-12-01 21:17:01 +0100

    added task 2

--- Praktikum11/Aufgabe1/renderSJDON.js
+++ Praktikum11/Aufgabe1/renderSJDON.js
@@ -1,48 +1,39 @@
-function renderSJDON(element, appRoot){
+function renderSJDON(element, appRoot) {
     appRoot.appendChild(generate(element))
 }
 
 
-function generate(list){
+function generate(list) {
     let root = undefined
 
     //Müssen durch alle Elemente durchgehen und prüfen was es werden muss
-    for(let i = 0; i < list.length; i++){
-        if (i == 0){
+    for (let i = 0; i < list.length; i++) {
+        if (i == 0) {
             root = document.createElement(list[i])
             continue;
         }
+        //Arrayhandler
+        if (Array.isArray(list[i])) {
+            let node = generate(list[i])
+            root.appendChild(node)
+            continue
+        }
 
         //Stringhandler
-        if (typeof(list[i]) === "string"){
+        if (typeof (list[i]) === "string") {
             let text = document.createTextNode(list[i])
             root.appendChild(text)
             continue
         }
 
         //Objecthandler
-        if (typeof(list[i]) === "object"){
-            for (element in list[i]){
-                root.setAttribute(element, list[i])
+        if (typeof (list[i]) === "object" && !Array.isArray(list[i])) {
+            for (item in list[i]) {
+                root.setAttribute(item, list[i][item])
             }
-            continue
-        }
-
-        //Arrayhandler
-        if (Array.isArray(list[i])){
-            let node = generate(list[i])
-            root.appendChild(node)
             continue
         }
     }
     return root
 }
 
-const element = ["div", {style: "background: salmon"},
- ["h1", "Hello World"],
- ["h2", {style: "text-align:right"}, "from our library"] ]
-
-
-let appRoot = document.getElementById("app")
-
-renderSJDON(element, appRoot)
